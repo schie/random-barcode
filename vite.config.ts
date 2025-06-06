221cf7c8f4aabaa5c1c9ffd74dc8f3f664010576
Dustin Schie <github@schie.io>
2025-06-05 22:12:40 -0400

    fix: display version with correct variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import pkg from "./package.json" assert { type: "json" };
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/random-barcode/",
+  define: {
+    APP_VERSION: JSON.stringify(pkg.version),
+  },
 });
